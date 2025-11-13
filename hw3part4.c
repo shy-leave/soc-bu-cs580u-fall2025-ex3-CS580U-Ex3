9cfa6b2972547109cc5ee76f3fab55741c0f4500
shy-leave <wxue@binghamton.edu>
2025-11-13 08:58:01 -0500

    Add files via upload

---
+++ hw3part4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void swap(char *x, char *y) {
+    char temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void permute(char *str, int l, int r) {
+    if (l == r)
+        printf("%s\n", str);
+    else {
+        for (int i = l; i <= r; i++) {
+            swap((str + l), (str + i));
+            permute(str, l + 1, r);
+            swap((str + l), (str + i)); // backtrack
+        }
+    }
+}
